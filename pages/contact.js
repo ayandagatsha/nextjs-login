7a2b4401977b61aa2df02206425767749e066bf6
ayanda <gatshaayanda@gmail.com>
2022-04-17 05:53:34 +0200

    v1

---
+++ pages/contact.js
@@ -0,0 +1,42 @@
+import { getSession } from 'next-auth/client';
+import { Fragment } from 'react';
+import Head from 'next/head';
+
+import ContactForm from '../components/contact/contact-form';
+
+
+
+export async function getServerSideProps(context) {
+  const session = await getSession({ req: context.req });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: { session },
+  };
+}
+
+
+function ContactPage() {
+  return (
+    <Fragment>
+      <Head>
+        <title>Contact Me</title>
+        <meta name='description' content='Send me your messages!' />
+      </Head>
+      <ContactForm />
+    </Fragment>
+  );
+}
+
+export default ContactPage;
+
+
+
